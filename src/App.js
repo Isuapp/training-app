b18778e76d04c2d228d04655b2c32590d2657973
karim <4bkarim.amaioua@gmail.com>
2022-06-28 17:36:41 +0100

    header started

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Route, Routes} from 'react-router-dom';
 
+import Header from './components/input/header/Header';
 import Landing from './containers/Landing';
 import Home from './containers/Home';
 import Filters from './containers/Filters';
@@ -13,17 +14,18 @@ import TrainingDetails from './containers/TrainingDetails';
 function App() {
   return (
     <div className="app">
-     <h1>Trainig app</h1>
-     <Routes>
-      <Route path='/' element={<Landing />}/>
-      <Route path='/home' element={<Home />}/>
-      <Route path='/register' element={<Register />}/>
-      <Route path='/login' element={<Login />}/>
-      <Route path='/training-details' element={<TrainingDetails />}/>
-      <Route path='/add-training' element={<AddTraining />}/>
-      <Route path='/edit-training' element={<EditTraining />}/>
-      <Route path='/filters' element={<Filters />}/>
-     </Routes>
+      <Header/>
+      <h1>Trainig app</h1>
+      <Routes>
+        <Route path='/' element={<Landing />}/>
+        <Route path='/home' element={<Home />}/>
+        <Route path='/register' element={<Register />}/>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/training-details' element={<TrainingDetails />}/>
+        <Route path='/add-training' element={<AddTraining />}/>
+        <Route path='/edit-training' element={<EditTraining />}/>
+        <Route path='/filters' element={<Filters />}/>
+      </Routes>
     </div>
   );
 }

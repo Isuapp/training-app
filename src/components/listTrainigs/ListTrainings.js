e09c5aa35354ef1910bdacd574e0e2f72eb43fe9
isuapp <isaacmarti.g@gmail.com>
2022-06-30 09:00:18 +0200

    Show training

--- src/components/listTrainigs/ListTrainings.js
+++ src/components/listTrainigs/ListTrainings.js
@@ -13,10 +13,12 @@ const ListTrainigs = ( ) => {
 
   const [token] = useToken();
   const [ trainings, setTrainings] = useState([]);
+  const [ training, setTraining] = useState([]);
 
   const [error, setError] = useState(null);
   const [success, setSuccess] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [showTraining, setShowTraining] = useState(false);
   
   const getTrainings = async () =>{
 
@@ -45,10 +47,35 @@ const ListTrainigs = ( ) => {
     }
   }
 
+  const showDetailTraining = async (idTraining)=>{
+
+    try {
+      
+      const res = await fetch(`http://localhost:4000/trainings/${idTraining}`,{
+        method:'GET',
+        headers:{
+          'content-type':'application/json',
+          Authorization: token,
+        }
+      });
+
+      const body = await res.json()
+      console.log(body.data.training);
+      const tr = body.data.training
+      if(res==='error') setError(body.message)
+      else {
+        setShowTraining(true);
+        console.log(showTraining);
+        setTraining([tr])
+      }
+    } catch (error) {
+      
+    }
+  }
   useEffect(()=>{getTrainings()},[])
 
   const showTrainings =   trainings.map(training=>(
-      <li key={training.id} className='training'>
+      <li key={training.id} className='training' onClick={()=>showDetailTraining(training.id)}>
         <div>  
           <figure> 
             <img src={`http://localhost:4000/${training.image}`}/*  alt={`image of training ${training.name}`} */ />
@@ -60,17 +87,31 @@ const ListTrainigs = ( ) => {
         </div>
       </li>
   ))
+
+/*   const trainingDetail = training.map(TR=>{
+      <article>
+        <p>{training.name}</p>
+        <p>{training.description}</p>
+        <button onClick={()=>{setShowTraining(false)}}>salir</button>
+      </article>
+  }) */
   
   
 
   return (
       <>
-        {loading? 
+        {loading &&
           <p>Cargando entrenamientos</p>
-          : 
+        }
+        {showTraining=== false ?
           <ul>
             {showTrainings}
           </ul>
+        :
+         <>
+         <p>training:{training.name}</p>
+         <button onClick={()=>setShowTraining(false)} >salir</button> 
+          </>
         }
         {error && <p className='error'>{error}</p>}
         {success && <p className='success'>{success}</p>}
--- src/containers/TrainingDetails.js
+++ src/containers/TrainingDetails.js
@@ -1,5 +1,14 @@
 const TrainingDetails = ()=>{
 
+    const getTraining = async ()=>{
+
+        try {
+            
+            const res = await fetch(`http://localhost:4000/trainings/${idTraining}`);
+        } catch (error) {
+            
+        }
+    }
     return(
         <main>
             <h2>TrainingDetails</h2>

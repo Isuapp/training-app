b109ff01de7cae1e8142c200f81eaffe2eba9e8e
isuapp <isaacmarti.g@gmail.com>
2022-07-03 18:37:55 +0200

    header with user problem solved

--- src/containers/TrainingsContainer.js
+++ src/containers/TrainingsContainer.js
@@ -1,11 +1,14 @@
 import ListTrainings from "../components/ListTrainings/ListTrainings";
+import { useToken } from "../context/TokenContext";
 import useTrainings from "../hooks/useTrainings";
+
 const Trainings = ()=>{
 
     const {trainings, loading, error} = useTrainings();
-
+    const [token] = useToken();
     if(loading) return <p>Loading...</p>
     if(error) return <p>{error}</p>
+    console.log(token)
     return(
         <main>
             <ListTrainings  trainings={trainings}/>
